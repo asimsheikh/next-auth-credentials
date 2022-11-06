0c6f858f8ba1d49a69585f0f6f9660cab1f3c09d
asheikh <asim.sardar.sheikh@gmail.com>
2022-11-06 17:08:32 +1100

    Adding a dashboard route to demonstrate client side routing

---
+++ pages/dashboard.tsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react';
+import { signIn, getSession } from 'next-auth/react';
+
+const Dashboard = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const securePage = async () => {
+      const session = await getSession();
+      if (!session) {
+        signIn();
+      } else {
+        setLoading(false);
+      }
+    };
+    securePage();
+  }, []);
+
+  if (loading) {
+    return <h2>Loading ...</h2>;
+  }
+
+  return <h1>Dashboard page</h1>;
+};
+
+export default Dashboard;
