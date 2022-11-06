951e543fa745466c1d9b9273dc917b5f455fe075
asheikh <asim.sardar.sheikh@gmail.com>
2022-11-06 14:49:04 +1100

    Adding a blog route, to test page authentication

---
+++ pages/blogs.tsx
@@ -0,0 +1,14 @@
+const Blogs = () => {
+  return (
+    <div>
+      <h1>Blogs</h1>
+      <ul>
+        <li>First item</li>
+        <li>Second item</li>
+        <li>Third item</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Blogs;
